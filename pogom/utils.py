73c0dc3bbd379b8a4015ffa0502e08abfbb0de5a
Onilton Maciel <oniltonmaciel@gmail.com>
2016-12-19 15:23:56 -0200

    Make altitude range param conform with the other params

--- pogom/utils.py
+++ pogom/utils.py
@@ -80,7 +80,7 @@ def get_args():
     parser.add_argument('-alt', '--altitude',
                         help='default altitude in meter',
                         type=int, default=13)
-    parser.add_argument('-altr', '--altitude_range',
+    parser.add_argument('-altr', '--altitude-range',
                         help='additional range for --altitude in meter',
                         type=int, default=1)
     parser.add_argument('-nj', '--no-jitter', help="Don't apply random -9m to +9m jitter to location.",
