f11a96c8805d825337d596d57ce6b58200d67444
sclo012 <sclo012@hotmail.com>
2016-12-31 11:18:49 -0800

    reverted changes to add spawnpoint

--- pogom/models.py
+++ pogom/models.py
@@ -1795,8 +1795,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                     'spawn_start': start_end[0],
                     'spawn_end': start_end[1],
 					'kind': sp['kind'],
-					'links': sp['links'],
-					'spawnpoint_id': p['spawnpoint_id']
+					'links': sp['links']
                 })
                 wh_update_queue.put(('pokemon', wh_poke))
 
