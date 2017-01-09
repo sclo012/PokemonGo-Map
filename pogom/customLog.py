580acec1d5bb5e8edb9b90ef8ff272ecf43f22ba
Samuel Andersson <samuel@samuelandersson.se>
2017-01-10 00:07:11 +0100

    Rewrite algorithm to find start/stop for 1x60 spawnpoints
    Previous algorithm used some kind of O(n^2) which became a huge problem
    for n=1000+. Now it will loop through all pokemons that were sighted 2
    or more times (for 1x60 spawnpoints, we can't determine start/end times
    with pokemons that were only scanned once). This way we can for each
    found pokemon expand the start/end window and that way find the range
    in O(n).
    
    Unit tests were added and as a result the args variable had to be moved
    from the root level of imports.

--- pogom/customLog.py
+++ pogom/customLog.py
@@ -2,7 +2,6 @@ from .utils import get_pokemon_rarity, get_pokemon_name
 from pogom.utils import get_args
 from datetime import datetime
 
-args = get_args()
 # Temporarily disabling because -o and -i is removed from 51f651228c00a96b86f5c38d1a2d53b32e5d9862.
 # IGNORE = None
 # ONLY = None
@@ -13,7 +12,7 @@ args = get_args()
 
 
 def printPokemon(id, lat, lng, itime):
-    if args.display_in_console:
+    if get_args().display_in_console:
         pokemon_name = get_pokemon_name(id).lower()
         pokemon_rarity = get_pokemon_rarity(id).lower()
         pokemon_id = str(id)
